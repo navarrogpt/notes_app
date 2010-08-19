67e68e32e6d731952e9a9801f778b46eb0b67d0d
unknown <sony@.(none)>
2010-08-19 21:31:54 +0800

    Implementation stage

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -5,7 +5,7 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
-	config.PreserveSessionOnFileBrowser = true;
+  config.PreserveSessionOnFileBrowser = true;
   // Define changes to default configuration here. For example:
   config.language = 'en';
   // config.uiColor = '#AADC6E';
@@ -14,10 +14,13 @@ CKEDITOR.editorConfig = function( config )
   
   config.height = '400px';
   config.width = '600px';
+
   
-  //config.resize_enabled = false;
+  config.resize_enabled = false;
   //config.resize_maxHeight = 2000;
   //config.resize_maxWidth = 750;
+  
+  config.skin = 'v2';
   
   //config.startupFocus = true;
   
@@ -28,15 +31,14 @@ CKEDITOR.editorConfig = function( config )
   
   config.toolbar_Easy =
     [
-        ['Source','-','Preview','Templates'],
-        ['Cut','Copy','Paste','PasteText','PasteFromWord',],
-        ['Maximize','-','About'],
-        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-        ['Styles','Format'],
-        ['Bold','Italic','Underline','Strike','-','Subscript','Superscript', 'TextColor'],
-        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
-        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['Link','Unlink','Anchor'],
-        ['Image','Embed','Flash','Attachment','Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
+        //['Source','-','Preview','Templates'],
+        //['Cut','Copy','Paste','PasteText','PasteFromWord',],
+        //['Maximize','-','About'],
+        //['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Font','FontSize','TextColor','Bold','Italic','Underline','JustifyLeft','JustifyCenter','JustifyRight']
+        //['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+        //['Link','Unlink','Anchor'],
+        //['Image','Embed','Flash','Attachment','Table','HorizontalRule','Smiley','SpecialChar','PageBreak']
     ];
+
 };
