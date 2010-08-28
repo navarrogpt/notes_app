c7e1e75ec09259f48e67fc55bc71b8802abf11b1
unknown <sony@.(none)>
2010-08-28 23:43:25 +0800

    Modified routes file

--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
-  map.root :controller => "notes"
+  map.root :controller => "index"
 
 end
